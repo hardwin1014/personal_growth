639695bdc68d279f532cbb0537bd33a04aa265f5
苍南 <914067438@qq.com>
2023-02-15 23:20:49 +0800

    feat: 实现深拷贝

--- 手搓代码/12_实现深拷贝.js
+++ 手搓代码/12_实现深拷贝.js
@@ -46,3 +46,20 @@ let obj1 = {
 
 const obj2 = _.cloneDeep(obj1)
 console.log(obj1.b.f === obj2.b.f)
+
+
+/**
+ * 3. 手写实现深拷贝函数
+ * */
+function deepCopy(object){
+    if(!object || typeof object !== "object") return;
+
+    let newObject = Array.isArray(object)? [] : {}
+
+    for(let key in object){
+        if(object.hasOwnProperty(key)){
+            newObject[key] = typeof object[key] === "object"? deepCopy(object[key]): object[key]
+        }
+    }
+    return newObject
+}
