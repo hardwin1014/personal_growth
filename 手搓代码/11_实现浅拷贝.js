e700b7d4433195257aa2ae41d892fde0ed5aa6e3
weixh <914067438@qq.com>
2023-02-17 09:34:18 +0800

    fix: 浅拷贝-object.assign

--- 手搓代码/11_实现浅拷贝.js
+++ 手搓代码/11_实现浅拷贝.js
@@ -17,10 +17,12 @@
 *     如果目标对象和源对象有同名属性，或者多个源对象有同名属性，则后面的属性会覆盖前面的属性
 *     如果该函数只有一个参数，当参数为对象时，直接返回该对象;当参数不是对象时,会先将参数转为对象然后返回。
 *     因为null和undefined 不能转换为对象，所以第一个参数不能为null和undefined，会报错
+*
+*     拷贝的对象中，string类型和symbol类型的属性都会被拷贝，而且不会跳过那些值为null或undefined的源对象。
 * */
 
 let target = { a: 1 }
-let object2 = { b: 2 }
+let object2 = { b: 2,d: undefined,e:null,f: Symbol("hello") }
 let object3 = { c: 3 }
 Object.assign(target, object2, object3)
 
