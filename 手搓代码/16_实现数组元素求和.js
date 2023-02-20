fbea4cd4521d3b9e57614bc4864bc2168b059b34
weixh <914067438@qq.com>
2023-02-20 09:45:24 +0800

    feat: 实现数组元素求和

--- 手搓代码/16_实现数组元素求和.js
+++ 手搓代码/16_实现数组元素求和.js
@@ -3,6 +3,7 @@ let sum = arr.reduce((total,i )=> total += i, 0)
 console.log(sum)
 
 let arr1 = [1,2,3,[[4,5],6],7,8,9]
+console.log(arr1.toString()) // 拍平
 let arrRes = arr1.toString().split(',').reduce((total, i) => {
     return total += Number(i)
 }, 0)
@@ -17,11 +18,10 @@ let arrRes2 = arr2.toString().split(",").reduce((total,i) => {
 console.log(arrRes2)
 
 
+// 递归
 let arr3 = [1,2,3,4,5,6]
 function add(arr){
-    if(arr.length === 1){
-        return arr[0]
-    }
+    if(arr.length === 1) return arr[0]
     return arr[0] + add(arr.slice(1))
 }
 
