6a05d2a9ad2e959ae6d64e09c28aa970e112ba02
weixh <914067438@qq.com>
2023-03-02 11:56:01 +0800

    feat: 循环打印红黄绿灯+实现每隔一秒打印1 2 3 4

--- 手搓代码/29_循环打印红黄绿.js
+++ 手搓代码/29_循环打印红黄绿.js
@@ -81,3 +81,20 @@ const taskRunner = async () => {
     await taskRunner()
 }
 taskRunner()
+
+
+// 实现每隔一秒打印1 2 3 4
+for(var i = 0; i < 5; i++){
+    (function (i){
+        setTimeout(() => {
+            console.log(i)
+        },i * 1000)
+    })(i)
+}
+
+// 使用let块级作用域
+for(let i = 0; i < 5; i++){
+    setTimeout(function () {
+        console.log(i)
+    },i * 1000)
+}
