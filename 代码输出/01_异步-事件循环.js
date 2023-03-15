d003133c4d7af1eb9aeb7d07a6187efe8449d0d3
weixh <914067438@qq.com>
2023-03-15 09:58:36 +0800

    feat: finally函数最终都会执行

--- 代码输出/01_异步-事件循环.js
+++ 代码输出/01_异步-事件循环.js
@@ -253,3 +253,46 @@ Promise.resolve()
   }).catch(function fail2(err) {
     console.log('fail2', err)
 })
+
+
+// 13. 代码输出结果
+Promise.resolve('1')
+    .then(res => {
+        console.log(res)
+    })
+    .finally(() => {
+        console.log('finally')
+    })
+
+Promise.resolve('2')
+  .finally(() => {
+      console.log('finally2')
+      return "我是finally2返回的值"
+  })
+  .then(res => {
+      console.log('finally后面的then函数', res)
+  })
+
+// 1  finally2  finally  finally2后面的then函数 2
+
+// .finally()一般用的很少，只要记住以下几点
+/*
+*   .finally() 方法不管Promise对象最后的状态如何都会执行
+*   .finally() 方法的回调函数不接受任何的参数，也就是说你在.finally() 函数中是无法知道Promise最终的状态是resolved还是rejected的
+*   .它最终返回的默认会是一个上一次的Promise对象值，不过如果抛出的是一个异常则返回异常的Promise对象
+*   .finally本质上是then方法的特例
+* */
+
+// .finally() 的错误捕获：
+Promise.resolve('1')
+  .finally(() => {
+      console.log('finally1')
+      throw new Error('我是finally中抛出的异常')
+  }).then(res => {
+    console.log('finally后面的then函数', res)
+}).catch(err => {
+    console.log('捕获错误', err)
+})
+
+// 'finally1'
+// '捕获错误' Error： 我是finally中抛出的异常
